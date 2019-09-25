ed0ff12683c91d38fadfa26e83c8613826423710
MiisterB <40004939+MiisterB@users.noreply.github.com>
2019-09-25 14:18:11 +0200

    Add cave generator

---
+++ 3D-Top-Down-Shooter-Sandbox/Assets/Scripts/EnnemyMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnnemyMovement : MonoBehaviour
+{
+    [Header("Associated objects :")]
+    public NavMeshAgent agent;
+    public Transform target;
+    public Animator animator;
+
+    [Header("Ennemy caracteristics :")]
+    [SerializeField] public float health;
+    [SerializeField] public float attackRate;
+    private float attackCountdown = 0f;
+
+    [Header("Ennemy stats :")]
+    public bool isAttacking = false;
+    public bool isChasing = false;
+
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = FindObjectOfType<Player>().transform;
+        animator = gameObject.GetComponent<Animator>();
+
+        animator.SetBool("Run Forward", true);
+        isChasing = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos = new Vector3(target.position.x, target.position.y, target.position.z);
+
+        float distanceToPlayer = Vector3.Distance(transform.position, target.position);
+
+        if(distanceToPlayer <= 4f)
+        {
+            isAttacking = true;
+            agent.isStopped = true;
+
+            if(attackCountdown <= 0)
+            {
+                isAttacking = true;
+                AttackPlayer();
+                attackCountdown = 1 / attackRate;
+            }
+            attackCountdown -= Time.deltaTime;
+        }
+        else
+        {
+            agent.isStopped = false;
+            ChasePlayer(targetPos);
+        }
+    }
+
+    void ChasePlayer(Vector3 targetPos)
+    {
+        animator.SetBool("Run Forward", true);
+        agent.SetDestination(targetPos);
+    }
+
+    void AttackPlayer()
+    {
+        if(isAttacking)
+        {
+            animator.SetBool("Run Forward", false);
+            animator.SetTrigger("Stab Attack");
+            isAttacking = false;
+        }
+    }
+}
---
+++ 3D-Top-Down-Shooter-Sandbox/Assets/Scripts/Player.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Shooting caracteristics :")]
+    [SerializeField] public float fireRate;
+    private float fireCountdown = 0;
+
+    [Header("Movement caracteristics :")]
+    [SerializeField] public float runSpeed;
+    [SerializeField] public float walkSpeed;
+    [SerializeField] public float offset;
+
+    [Header("Components :")]
+    public Rigidbody rb;
+    public Animator animator;
+    public Camera camera;
+    public Transform shootPoint;
+    public GameObject projectile;
+
+    private Vector3 movement;
+    private Vector3 mousePos;
+    private Vector3 pointToLook;
+
+    [Header("Stats :")]
+    public bool isMoving = false;
+    public bool isRunning = false;
+    public bool isShooting = false;
+
+    private List<ParticleCollisionEvent> collisionEvents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisionEvents = new List<ParticleCollisionEvent>();
+        camera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input movements
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isRunning = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isRunning = false;
+        }
+
+        //Input rotation
+        Ray cameraRay = camera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.red);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+
+        //Input shooting while mooving
+        if (Input.GetMouseButton(0) && isMoving && !isRunning)
+        {
+            isShooting = true;
+            if (fireCountdown <= 0)
+            {
+                ShootProjectile();
+                fireCountdown = 1 / fireRate;
+            }
+            fireCountdown -= Time.deltaTime;
+        }
+
+        //Input shooting while idle
+        else if (Input.GetMouseButton(0) && !isMoving && !isRunning)
+        {
+            isShooting = true;
+            if (fireCountdown <= 0)
+            {
+                ShootProjectile();
+                fireCountdown = 1 / (fireRate * 2);
+            }
+            fireCountdown -= Time.deltaTime;
+        }
+        else
+        {
+            isShooting = false;
+        }
+
+        //Movement detection
+        if (movement.x != 0 || movement.z != 0)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        //Animator management
+        animator.SetFloat("movementAxisX", movement.x);
+        animator.SetFloat("movementAxisZ", movement.z);
+        animator.SetBool("isMoving", isMoving);
+        animator.SetBool("isRunning", isRunning);
+        animator.SetBool("isShooting", isShooting);
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        if (!isRunning)
+        {
+            rb.MovePosition(rb.position + movement * walkSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            Vector3 newPosition = new Vector3(pointToLook.x, pointToLook.y + offset, pointToLook.z);
+            transform.position = Vector3.MoveTowards(transform.position, newPosition, runSpeed * Time.deltaTime);
+        }
+    }
+
+    void ShootProjectile()
+    {
+        GameObject projectileGO = Instantiate(projectile, shootPoint.transform.position, shootPoint.rotation);
+        Destroy(projectileGO, 1.0f);
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ParticlePhysicsExtensions.GetCollisionEvents(projectile.GetComponent<ParticleSystem>(), other, collisionEvents);
+
+        foreach(ParticleCollisionEvent colission in collisionEvents)
+        {
+            Debug.Log(colission);
+        }
+    }
+}
+
